fa063ce5781a1b223b06cc6d674eba247625eb81
312spartan <ian.royer31@gmail.com>
2025-07-31 21:11:01 -0700

    Adding todays JS practice

---
+++ js-recap/project-03/script.js
@@ -0,0 +1,46 @@
+console.log("Hello world!");
+
+
+
+let sum = 10 + 20;
+console.log(sum);
+
+let outcome = 5 * 2;
+console.log(outcome);
+
+let dividend = 10 / 2;
+console.log(dividend);
+
+const answer = 10 > 2;
+
+const value = 8 - 12;
+console.log(value);
+
+const remainder = 10 % 3;
+console.log(remainder);
+
+const secondAnswer = 5 < 9;
+
+const equals = 10 == 10;
+
+const strict = 5 === 5;
+
+const notEqual = 7 !== 7;
+
+if (10 > 2) {
+    console.log("Yes 10 > 2!");
+} else if (10 < 2) {
+    console.log("Error: This is incorrect.");
+}
+
+
+
+console.log(answer);
+
+console.log(secondAnswer);
+
+console.log(equals);
+
+console.log(strict);
+
+console.log(notEqual);
