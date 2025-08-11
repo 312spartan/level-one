d4e785eba2b147f1554e0b025ab03cff0656a639
312spartan <ian.royer31@gmail.com>
2025-08-10 21:00:34 -0700

    Adding todays practice

---
+++ js-recap/project-08/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World!!");
+
+
+let guestlist = [];
+
+const guestInput = document.getElementById("guestInput");
+const addBtn = document.getElementById("addBtn");
+const removeBtn = document.getElementById("removeBtn");
+const guestList = document.getElementById("guestList");
+const totalCount = document.getElementById("totalCount");
